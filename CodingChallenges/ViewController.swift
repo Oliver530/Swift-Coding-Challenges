39bd502fe55f04e1d09c773145069014ad6289c7
Oliver Gerhardt <oli.gerhardt@gmail.com>
2019-03-18 19:02:35 +0100

    Initial Commit

---
+++ CodingChallenges/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodingChallenges
+//
+//  Created by Oliver Gerhardt on 18.03.19.
+//  Copyright © 2019 Oliver Gerhardt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
